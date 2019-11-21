032da7ce91b83d4b76cbf0491a92ec9658120164
Isaias Arellano <iarellano@nearbpo.com>
2019-11-21 16:18:46 -0600

    Work in progress

--- src/main/resources/META-INF/archetype-post-generate.groovy
+++ src/main/resources/META-INF/archetype-post-generate.groovy
@@ -1,7 +1,151 @@
+import io.swagger.v3.oas.models.Operation
+import io.swagger.v3.parser.OpenAPIV3Parser
+import io.swagger.v3.oas.models.OpenAPI
+import org.w3c.dom.Document
+import org.w3c.dom.Element
+
+import javax.xml.parsers.DocumentBuilder
+import javax.xml.parsers.DocumentBuilderFactory
+import javax.xml.transform.OutputKeys
+import javax.xml.transform.Transformer
+import javax.xml.transform.TransformerFactory
+import javax.xml.transform.dom.DOMSource
+import javax.xml.transform.stream.StreamResult
+@Grapes([
+    @Grab(group='org.openapitools', module='openapi-generator', version='4.2.1'),
+    @Grab(group='com.fasterxml.jackson.core', module='jackson-databind', version='2.9.8'),
+    @Grab(group='com.fasterxml.jackson.dataformat', module='jackson-dataformat-yaml', version='2.9.8'),
+    @Grab(group='com.fasterxml.jackson.datatype', module='jackson-datatype-jsr310', version='2.9.8'),
+//    @Grab(group='io.swagger.parser.v3', module='swagger-parser', version='2.0.16'),
+    @Grab(group='org.apache.commons', module='commons-lang3', version='3.4')
+])
+
 import java.nio.file.Files
 import java.nio.file.StandardCopyOption
 
+import com.fasterxml.jackson.core.JsonFactory
+import com.fasterxml.jackson.databind.DeserializationFeature
+import com.fasterxml.jackson.databind.ObjectMapper
+import com.fasterxml.jackson.dataformat.yaml.YAMLFactory
+import com.fasterxml.jackson.dataformat.yaml.YAMLMapper
+
+import org.openapitools.codegen.CliOption
+import org.openapitools.codegen.ClientOptInput
+import org.openapitools.codegen.CodegenConfig
+import org.openapitools.codegen.CodegenConstants
+import org.openapitools.codegen.DefaultGenerator
+import org.openapitools.codegen.auth.AuthParser
+import org.openapitools.codegen.config.CodegenConfigurator
+import org.openapitools.codegen.config.GlobalSettings
+
+def readDocument(def xmlFile) {
+    DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance()
+    DocumentBuilder dBuilder = dbFactory.newDocumentBuilder()
+    Document doc = dBuilder.parse(xmlFile)
+    doc.getDocumentElement().normalize()
+    return doc
+}
+
+def writeDocument(def doc, def xmlFile) {
+    doc.getDocumentElement().normalize()
+    TransformerFactory transformerFactory = TransformerFactory.newInstance()
+    Transformer transformer = transformerFactory.newTransformer()
+    transformer.setOutputProperty("{http://xml.apache.org/xslt}indent-amount", "4")
+    DOMSource source = new DOMSource(doc)
+    StreamResult result = new StreamResult(xmlFile)
+    transformer.setOutputProperty(OutputKeys.INDENT, "yes")
+    transformer.transform(source, result)
+}
+
+def apigeeCondition(def path, def method) {
+    return String.format('(proxy.pathsuffix MatchesPath "%1$s") and (request.verb = "%2$s")',
+            path.replaceAll("\\{.*?\\}", "*"), method.toUpperCase()
+    )
+}
+
+def addFlowCondition(Document doc, String path, String method, String operationId, String summary) {
+    Element desc = doc.createElement("Description")
+    desc.setTextContent(summary)
+    Element condition = doc.createElement("Condition")
+    condition.setTextContent(apigeeCondition(path, method))
+    Element flow = doc.createElement("Flow")
+    flow.setAttribute("name", operationId)
+    flow.appendChild(desc)
+    flow.appendChild(doc.createElement("Request"))
+    flow.appendChild(doc.createElement("Response"))
+    flow.appendChild(condition)
+    doc.getElementsByTagName("Flows").item(0).appendChild(flow)
+}
+
+def generateMockEndpoint(def spec) {
+    CodegenConfigurator configurator = new  CodegenConfigurator()
+    configurator.setInputSpec(spec)
+    configurator.setGenerateAliasAsModel(true)
+    configurator.setGeneratorName("nodejs-express-server")
+    configurator.setVerbose(true)
+    configurator.setLogToStderr(true)
+    configurator.setOutputDir(new File(request.getOutputDirectory() + File.separator + "mock").getAbsolutePath())
+    configurator.setValidateSpec(false)
+    GlobalSettings.clearProperty(CodegenConstants.MODELS)
+    final ClientOptInput input = configurator.toClientOptInput()
+    final CodegenConfig config = input.getConfig()
+    new DefaultGenerator().opts(input).generate()
+}
+
+def readSpec(def specLocation) {
+    def specUrl = specLocation.matches("^(https?|file)://.*") ? new URL(specLocation) : new File(specLocation).toURL()
+
+    def bis = new BufferedInputStream(specUrl.openStream())
+    byte[] buffer = new byte[1024]
+    int bytesRead = 0
+    def baos = new ByteArrayOutputStream()
+    while ((bytesRead = bis.read(buffer, 0, buffer.length)) != -1) {
+        baos.write(buffer, 0, bytesRead)
+    }
+    bis.close()
+    return baos.toByteArray()
+}
+
+def getSpecVersion(def specLocation, def specBytes) {
+    ObjectMapper mapper = new ObjectMapper(specLocation.endsWith(".json") ? new JsonFactory() : new YAMLFactory())
+    mapper.findAndRegisterModules()
+    mapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false)
+    return mapper.readValue(specBytes, Map.class).get("openapi")
+}
+
+def ensureYAML(def specLocation, def bytes) {
+    return specLocation.endsWith(".json") ? new YAMLMapper().writeValueAsString(new ObjectMapper().readTree(new String(bytes))) : new String(bytes)
+}
+
+String spec = request.getProperties().get("spec")
+def specBytes = readSpec(spec)
+def version = getSpecVersion(spec, specBytes)
+def specDocStr = ensureYAML(spec, specBytes)
+
 File outputDir = new File(request.getOutputDirectory() + File.separator + artifactId)
+
+File proxyFile = new File(outputDir, "src/main/gateway/template-v1/apiproxy/proxies/default.xml")
+File targetFile = new File(outputDir, "src/main/gateway/template-v1/apiproxy/targets/default.xml")
+
+Document proxyDoc = readDocument(proxyFile)
+Document targetDoc = readDocument(targetFile)
+
+if (version.matches("3\\..*")) {
+    OpenAPI openAPI = new OpenAPIV3Parser().readContents(specDocStr).openAPI
+    for (def pathSuffix: openAPI.paths.keySet()) {
+        for (def httpMethod: openAPI.paths.get(pathSuffix).readOperationsMap().keySet()) {
+            Operation operation = openAPI.paths.get(pathSuffix).readOperationsMap().get(httpMethod)
+            addFlowCondition(proxyDoc, pathSuffix, httpMethod.toString(), operation.getOperationId(), operation.getSummary())
+            addFlowCondition(targetDoc, pathSuffix, httpMethod.toString(), operation.getOperationId(), operation.getSummary())
+        }
+    }
+}
+
+writeDocument(proxyDoc, proxyFile)
+writeDocument(targetDoc, targetFile)
+
+
+/*
 
 // Move contents from root dir to project.basedir
 def rootDir = new File(outputDir, "root")
@@ -12,6 +156,8 @@ for (File rootFile: rootDir.listFiles()) {
     }
 }
 rootDir.delete()
+
+
 
 // Rename apiproxy directory from template-v1 to ${apiproxyName}
 def apiproxyName = request.getProperties().get("apiproxyName") as String
@@ -67,3 +213,4 @@ new File(testJavaDir.getParent(), "resources").mkdirs()
 def pw = new PrintWriter(new File(outputDir, "autogenerate-project.properties"))
 request.getProperties().list(pw)
 pw.close()
+*/
--- test.sh
+++ test.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
+rm -rf ~/.m2/repository/org/apache/maven
+rm ~/.m2/repository/archetype-catalog.xml
+
 mvn clean install
 
 current_dir=`pwd`
@@ -7,15 +10,20 @@ artefactId="$1"
 targetdir="tmp"
 
 cd $targetdir
-rm -rf $artefactId
+rm -rf *
+cp ../petstore.yaml ./
+cp ../petstore.json ./
 
 mvn archetype:generate                                  \
   -DarchetypeGroupId=com.github.iarellano               \
-  -DarchetypeArtifactId=single-apiproxy-archetype       \
-  -DarchetypeVersion=1.0-SNAPSHOT                       \
+  -DarchetypeArtifactId=openapi2apigee-archetype        \
+  -DarchetypeVersion=1.0.0-SNAPSHOT                     \
   -DgroupId=com.deploy.test                             \
-  -DartifactId=$artefactId                             \
+  -DartifactId=$artefactId                              \
   -DtestOrganization=asantosdevportal-eval              \
-  -DinteractiveMode=false
+  -DinteractiveMode=true                                \
+  -Dspec=./petstore.json
+#  -Dspec=https://raw.githubusercontent.com/OAI/OpenAPI-Specification/master/examples/v3.0/petstore.yaml
+#  -Dspec=https://raw.githubusercontent.com/OAI/OpenAPI-Specification/master/examples/v2.0/yaml/petstore.yaml
+#  -Dspec=https://raw.githubusercontent.com/OAI/OpenAPI-Specification/master/examples/v2.0/yaml/petstore.yaml
 
-cd $current_dir
